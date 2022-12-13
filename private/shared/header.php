794769036fe0cb07cb3c1abae773fe321c37cafe
Iqgor <herrebrughigor@gmail.com>
2022-12-13 12:11:01 +0100

    bezig met opdracht 2 week2

--- private/shared/header.php
+++ private/shared/header.php
@@ -1,11 +1,27 @@
+<?php
+
+$root = substr($_SERVER['SCRIPT_NAME'],
+    0,
+    strpos($_SERVER['SCRIPT_NAME'], '/public') + 7);
+define("ROOT_URL", $root);
+
+function url_path($path) {
+    if($path[0] != '/') {
+        $path = "/" . $path;
+    }
+    return ROOT_URL . $path;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="../../public/css/style.css">
+    <link rel="stylesheet" href="<?php echo "";?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+  <link rel="stylesheet" href="">
     <title>Startpagina</title>
 </head>
 <body>
