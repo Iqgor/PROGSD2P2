9cd46674686d97ba3ecb1d2a82900e5bd0c04116
Iqgor <herrebrughigor@gmail.com>
2022-12-14 12:28:49 +0100

    week 2.1 af

--- public/gesplitst.php
+++ public/gesplitst.php
@@ -4,11 +4,26 @@
   Standaard tekst
 
 </h1>
-<form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+<form action="" method="post">
   Name: <input type="text" name="name"><br>
   E-mail: <input type="text" name="email"><br>
-  Comentaar: <textarea name="comentaar" id="" cols="30" rows="10"></textarea>
-  <input type="submit">
+  Comentaar: <textarea name="coment" id="" cols="30" rows="10"></textarea>
+  <input type="submit" name="submit" value="Submit">
 </form>
+
+<?php 
+  if(($_SERVER['REQUEST_METHOD'])== 'post'){
+
+    $name = trim($_POST['name']);
+    $email = trim($_POST['email']);
+    $coment = trim($_POST['coment']);
+
+    echo $name;
+    echo $email;
+    echo $coment;
+    
+  };
+
+?>
 
 <?php include "../private/shared/footer.php"; ?>
