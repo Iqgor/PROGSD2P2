ca3364e27342633c63544e35a82a36c30d057292
Iqgor <herrebrughigor@gmail.com>
2022-12-14 12:21:01 +0100

    week 2 af

--- private/shared/header.php
+++ private/shared/header.php
@@ -18,7 +18,7 @@ function url_path($path) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="<?php echo "";?>">
+    <link rel="stylesheet" href="<?php echo url_path("css/style.css")?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="">
--- public/gesplitst.php
+++ public/gesplitst.php
@@ -2,6 +2,13 @@
 
 <h1>
   Standaard tekst
+
 </h1>
+<form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+  Name: <input type="text" name="name"><br>
+  E-mail: <input type="text" name="email"><br>
+  Comentaar: <textarea name="comentaar" id="" cols="30" rows="10"></textarea>
+  <input type="submit">
+</form>
 
 <?php include "../private/shared/footer.php"; ?>
